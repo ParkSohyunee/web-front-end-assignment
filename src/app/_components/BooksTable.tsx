3e2a4254d69e5aec63c98b11dde7728d94e8bfb2
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-12-12 01:29:42 +0900

    feat: 책 목록 페이지 페이지네이션 기능 구현

--- src/app/_components/BooksTable.tsx
+++ src/app/_components/BooksTable.tsx
@@ -1,25 +1,45 @@
 'use client';
 
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
-import { BookType } from '../_lib/types/book';
+import { BookType } from '@/app/_lib/types/book';
 
 export default function BooksTable() {
+  const searchParams = useSearchParams();
   const [books, setBooks] = useState<BookType[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+
+  const page = searchParams.get('page');
+  const limit = searchParams.get('limit');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/api/books');
+        const response = await fetch(`/api/books?page=${page}&limit=${limit}`);
         const result = await response.json();
-        setBooks(result);
+        setBooks(result.data);
+        setTotalCount(result.totalCount);
       } catch (error) {
         console.error('error:', error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [page]);
+
+  // --페이지네이션
+  const dataSize = 10; // 한 페이지당 불러올 데이터
+  const pageSize = 3; // 화면에 보여질 페이지 개수
+  const totalPages = Math.ceil(totalCount / dataSize); // 총 페이지 개수
+
+  const currentPage = page && parseInt(page) > 0 ? parseInt(page) : 1; // 현재 페이지
+  const pageGroup = Math.ceil(currentPage / pageSize); // 화면에 보여질 페이지 그룹
+
+  // 화면에 보여질 데이터의 첫번째 페이지 번호와 마지막 페이지 번호 계산
+  const firstPageNumber = (pageGroup - 1) * pageSize + 1;
+  const lastPageNumber = pageGroup * pageSize > totalPages ? totalPages : pageGroup * pageSize;
 
   return (
     <section>
@@ -31,6 +51,19 @@ export default function BooksTable() {
           </li>
         ))}
       </ul>
+
+      {pageGroup !== 1 && <Link href={`?page=${firstPageNumber - 1}&limit=${dataSize}`}>이전</Link>}
+      {Array.from(
+        { length: lastPageNumber - firstPageNumber + 1 },
+        (_, i) => firstPageNumber + i,
+      ).map((value, index) => (
+        <Link key={index} href={`?page=${firstPageNumber + index}&limit=${dataSize}`}>
+          {value}
+        </Link>
+      ))}
+      {pageGroup * pageSize < totalPages && (
+        <Link href={`?page=${lastPageNumber + 1}&limit=${dataSize}`}>다음</Link>
+      )}
     </section>
   );
 }
--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,12 +1,28 @@
+import { NextRequest } from 'next/server';
 import { promises } from 'fs';
 import path from 'path';
 
-export async function GET() {
+import { BookType } from '@/app/_lib/types/book';
+
+export async function GET(request: NextRequest) {
+  const params = request.nextUrl.searchParams.get('page');
+  const limit = request.nextUrl.searchParams.get('limit');
+
+  const page = params !== 'null' ? params : 1;
+  const size = limit !== 'null' ? Number(limit) : 10; // 한 페이지당 불러올 데이터
+
+  const startIndex = (Number(page) - 1) * size;
+  const endIndex = startIndex + size;
+
   try {
     const filePath = path.join(process.cwd(), 'data', 'books.json');
     const data = await promises.readFile(filePath, 'utf-8'); // string
-    const books = JSON.parse(data); // object
-    return Response.json(books);
+    const books: BookType[] = JSON.parse(data); // object
+
+    return Response.json({
+      data: books.slice(startIndex, endIndex),
+      totalCount: books.length,
+    });
   } catch (error) {
     return new Response(JSON.stringify({ message: '데이터 조회 실패' }), { status: 500 });
   }
