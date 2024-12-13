5a681fbd4420561d65feec9fb72c5cafadfa8840
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-12-13 21:30:34 +0900

    fix: useSearchParams 사용 관련 빌드 에러 해결을 위해 서버컴포넌트에 Suspense 추가

--- src/app/(route)/books/page.tsx
+++ src/app/(route)/books/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react';
+
 import BooksTable from '@/app/_components/BooksTable';
 
 export default async function BooksPage() {
   return (
     <main>
-      <BooksTable />
+      <Suspense fallback={<div>로딩</div>}>
+        <BooksTable />
+      </Suspense>
     </main>
   );
 }
