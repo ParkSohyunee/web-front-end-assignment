917ef6d703e40799ba84820081269693403b192f
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-12-13 10:01:50 +0900

    feat: 책 상세 정보 조회 API 구현

---
+++ src/app/api/books/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest } from 'next/server';
+import { BookType } from '@/app/_lib/types/book';
+
+/** 책 상세 조회 API */
+export async function GET(request: NextRequest, { params }: { params: Promise<{ id: number }> }) {
+  const { id: bookId } = await params;
+
+  try {
+    const response = await fetch(`http://localhost:3001/books/${bookId}`);
+    const book: BookType = await response.json();
+
+    if (response.ok) {
+      return new Response(JSON.stringify(book));
+    } else {
+      return new Response(JSON.stringify({ message: '일치하는 데이터 없음' }), { status: 404 });
+    }
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify({ message: '데이터 조회 실패' }), { status: 500 });
+  }
+}
